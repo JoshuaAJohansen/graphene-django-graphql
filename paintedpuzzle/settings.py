f9f22138ead634c8839f038dc7e985897938de39
Josh Johansen <josh@johansen.us.com>
2019-08-17 01:16:44 -0700

    updated django to remediate security vulnerability.'

--- paintedpuzzle/settings.py
+++ paintedpuzzle/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!-#i#otef2r3po48r(xlowe(yj-$lb0+(uu6c&^kaw*)5s_g@2'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
